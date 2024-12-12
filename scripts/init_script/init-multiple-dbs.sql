c55f50a08d8485487743ef68ecdc8afba51ec51d
teetegert <teetegert@ut.ee>
2024-12-12 18:19:21 +0200

    postgres raw data

---
+++ scripts/init_script/init-multiple-dbs.sql
@@ -0,0 +1,9 @@
+-- Create the raw_data database
+CREATE DATABASE raw_data;
+
+-- Optional: Grant privileges
+-- GRANT ALL PRIVILEGES ON DATABASE raw_data TO airflow;
+
+
+-- to access raw_data database
+-- psql -U airflow -d raw_data -h localhost
